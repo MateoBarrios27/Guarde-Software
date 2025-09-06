b0b458af02c01b66ce9dad395ceef662d5dad6b5
Bruno Bassini <brunobassini86@gmail.com>
2025-09-05 23:57:23 -0300

    Feat: Add Quartz and async methods in AccessDB plus impementation in DAOAccountMovement to Jobs

--- GuardeSoftwareAPI/Dao/AccessDB.cs
+++ GuardeSoftwareAPI/Dao/AccessDB.cs
@@ -25,6 +25,8 @@ namespace GuardeSoftwareAPI.Dao
             }
         }
 
+        //This method is useful for SELECT queries
+        //Returns a DataTable with the results of the query
         public DataTable GetTable(string tableName, string consult, SqlParameter[] parameters = null)
         {
             using (SqlConnection connection = new SqlConnection(routeDB))
@@ -47,6 +49,8 @@ namespace GuardeSoftwareAPI.Dao
 
         }
 
+        //This method realizes commands that do not return results (INSERT, UPDATE, DELETE)
+        //Returns the number of affected rows
         public int ExecuteCommand(string query, SqlParameter[] parameters)
         {
             using (SqlConnection connection = new SqlConnection(routeDB))
@@ -71,6 +75,51 @@ namespace GuardeSoftwareAPI.Dao
                     {
                         throw new Exception("Error executing the query", ex);
                     }
+                }
+            }
+        }
+
+        //New async methods
+        //Actually, they are used for jobs with Quartz
+        //This method realizes commands that do not return results (INSERT, UPDATE, DELETE)
+        //Returns the number of affected rows
+        public async Task<int> ExecuteCommandAsync(string query, SqlParameter[] parameters)
+        {
+            using (var connection = new SqlConnection(routeDB))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    await connection.OpenAsync();
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    return rowsAffected;
+                }
+            }
+        }
+
+        //This method is useful for getting the last inserted id
+        //Returns an object that can be cast to the desired type
+        //Example of use:
+        //string query = "INSERT INTO table_name(column1, column2) VALUES(@value1, @value2); SELECT SCOPE_IDENTITY();";
+        //object result = await accessDB.ExecuteScalarAsync(query, parameters);
+        public async Task<object> ExecuteScalarAsync(string query, SqlParameter[] parameters = null)
+        {
+            using (var connection = new SqlConnection(routeDB))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    await connection.OpenAsync();
+                    object result = await command.ExecuteScalarAsync();
+                    return result;
                 }
             }
         }
--- GuardeSoftwareAPI/Dao/DaoAccountMovement.cs
+++ GuardeSoftwareAPI/Dao/DaoAccountMovement.cs
@@ -6,8 +6,8 @@ using GuardeSoftwareAPI.Entities;
 
 namespace GuardeSoftwareAPI.Dao
 {
-	public class DaoAccountMovement
-	{
+    public class DaoAccountMovement
+    {
         private readonly AccessDB accessDB;
 
         public DaoAccountMovement(AccessDB _accessDB)
@@ -22,7 +22,8 @@ namespace GuardeSoftwareAPI.Dao
             return accessDB.GetTable("account_movements", query);
         }
 
-        public DataTable GetAccountMovById(int id) {
+        public DataTable GetAccountMovById(int id)
+        {
 
             string query = "SELECT movement_id, rental_id,movement_date,movement_type,concept,amount, payment_id FROM account_movements WHERE movement_id = @movement_id";
 
@@ -59,10 +60,54 @@ namespace GuardeSoftwareAPI.Dao
                 new SqlParameter("@payment_id",SqlDbType.Int) { Value = accountMovement.PaymentId }
             };
 
-            string query = "INSERT INTO account_movements(rental_id,movement_date,movement_type,concept,amount, payment_id)"
-            + "VALUES(@rental_id,@movement_date,@movement_type,@concept,@amount, @payment_id)";
+            string query = "INSERT INTO account_movements(rental_id, movement_date, movement_type, concept, amount, payment_id)"
+            + "VALUES(@rental_id, @movement_date, @movement_type, @concept, @amount, @payment_id)";
 
             return accessDB.ExecuteCommand(query, parameters) > 0;
+        }
+
+        //This method checks if a debit entry exists for the current month for a given rental ID
+        //It returns true if the entry debit exists, otherwise false.
+        //Is used in DebitService and is a verification
+        public async Task<bool> CheckIfDebitExistsForCurrentMonthAsync(int rentalId)
+        {
+            string query = @"
+                SELECT COUNT(1) 
+                FROM account_movements 
+                WHERE rental_id = @rentalId 
+                  AND movement_type = 'DEBITO' 
+                  AND MONTH(movement_date) = MONTH(GETDATE()) 
+                  AND YEAR(movement_date) = YEAR(GETDATE());";
+
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@rentalId", SqlDbType.Int) { Value = rentalId }
+            };
+
+            object result = await accessDB.ExecuteScalarAsync(query, parameters);
+
+            return Convert.ToInt32(result) > 0;
+        }
+
+         public async Task CreateDebitAsync(AccountMovement debit)
+        {
+            string query = @"
+                INSERT INTO account_movements (rental_id, movement_date, movement_type, concept,amount, payment_id)
+                VALUES (@rental_id, @movement_date, @movement_type, @concept, @amount, @payment_id);";
+
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@rentalId", SqlDbType.Int) { Value = debit.RentalId},
+                new SqlParameter("@movementDate", SqlDbType.DateTime) { Value = debit.MovementDate },
+                new SqlParameter("@movementType", SqlDbType.VarChar) { Value = debit.MovementType },
+                new SqlParameter("@concept", SqlDbType.VarChar) { Value = debit.Concept },
+                new SqlParameter("@amount", SqlDbType.Decimal) { Value = debit.Amount },
+                // Validation for possible nulls for optional foreign key
+                new SqlParameter("@paymentId", SqlDbType.Int) { Value = (object)debit.PaymentId ?? DBNull.Value }
+            };
+            
+            
+            await accessDB.ExecuteCommandAsync(query, parameters);
         }
     }         
 }  
