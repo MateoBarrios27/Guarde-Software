9ceb11b0b6f955cd46b1925708cd43ae7660283f
MateoBarrios27 <mateobabarrios@gmail.com>
2025-10-06 20:01:10 -0300

    feat(frontend/dashboard): redesign + history table + payment modal for (UI)

--- GuardeSoftwareClient/src/app/pages/dashboard/dashboard.component.ts
+++ GuardeSoftwareClient/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { PendingRentalDTO } from '../../core/dtos/rental/pendingRentalDTO';
+import { PendingRentalDTO } from '../../core/dtos/rental/PendingRentalDTO';
 import { RentalService } from '../../core/services/rental-service/rental.service';
+import { PaymentService } from '../../core/services/payment-service/payment.service';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../../shared/components/icon/icon.component';
+import { Payment } from '../../core/models/payment';
+import { FormsModule } from '@angular/forms';
+import { CreatePaymentDTO } from '../../core/dtos/payment/CreatePaymentDTO';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, IconComponent],
+  imports: [CommonModule, IconComponent, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -14,9 +18,28 @@ export class DashboardComponent {
 
   pendingRentals: PendingRentalDTO[] = [];
 
-  constructor(private rentalService: RentalService) {}
+  payments: Payment[] = [];
+
+  showPaymentModal = false;
+  selectedClientName = '';
+  selectedPaymentIdentifier = 0;
+
+  paymentDto: CreatePaymentDTO = {
+    clientId: 0,
+    rentalId: 0,
+    movementType: 'CREDITO',
+    concept: 'Pago de alquiler',
+    amount: 0
+  };
+ 
+  constructor(private rentalService: RentalService, private paymentService: PaymentService) {}
 
   ngOnInit(): void {
+    this.LoadPedingRentals();
+    this.LoadPayments();
+  }
+
+  LoadPedingRentals(): void{
     this.rentalService.getPendingRental().subscribe({
       next: (data) => {
         this.pendingRentals = data;
@@ -25,4 +48,29 @@ export class DashboardComponent {
       error: (err) => console.error('Error al cargar pendientes:', err)
     });
   }
+
+  LoadPayments(): void{
+    this.paymentService.getPayment().subscribe({
+      next: (data) => {
+        this.payments = data;
+      },
+       error: (err) => console.error('Error al cargar payments:', err)
+    });
+  }
+
+  openPaymentModalWith(item: PendingRentalDTO) {
+    this.paymentDto = {
+      clientId: item.clientId ?? 0,
+      rentalId: item.id ?? 0,
+      movementType: 'CREDITO',
+      concept: `Pago alquiler`,
+      amount: item.currentRent ?? 0
+    };
+    this.selectedClientName = item.clientName ?? '';
+    this.selectedPaymentIdentifier = item.paymentIdentifier ?? '';
+    this.showPaymentModal = true;
+  }
+
+  closePaymentModal() { this.showPaymentModal = false; }
+  
 }
