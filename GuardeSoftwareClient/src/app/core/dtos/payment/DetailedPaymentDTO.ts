7acc2e2f0f65debcb317b6891a1fb6462c15b735
MateoBarrios27 <mateobabarrios@gmail.com>
2025-10-17 16:07:38 -0300

    feat(frontend/payments): modification to receive the warehouse name

--- GuardeSoftwareClient/src/app/core/dtos/payment/DetailedPaymentDTO.ts
+++ GuardeSoftwareClient/src/app/core/dtos/payment/DetailedPaymentDTO.ts
@@ -4,6 +4,7 @@ export interface DetailedPaymentDTO{
     clientName: string;
     paymentIdentifier: string;
     lockerIdentifier: string;
+    warehouse_name: string;
     amount: number;
     paymentDate: Date;
     paymentMethodName: string;
