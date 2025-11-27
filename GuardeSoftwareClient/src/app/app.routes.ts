91140d384e9beaca6780bbddfeaa44d8ff06bf3d
MateoBarrios27 <mateobabarrios@gmail.com>
2025-11-27 19:12:03 +0000

    feat(frontend/login): add authguard for all the paths

--- GuardeSoftwareClient/src/app/app.routes.ts
+++ GuardeSoftwareClient/src/app/app.routes.ts
@@ -7,20 +7,26 @@ import { CommunicationsComponent } from './pages/communications/communications.c
 import { ReportsComponent } from './pages/reports/reports.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   // Redirect the root path to /clients by default
-  { path: '', redirectTo: '/clients', pathMatch: 'full' }, 
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent, data: { title: 'Login' } },
   
-  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
-  { path: 'clients', component: ClientsComponent, data: { title: 'Clientes' } },
-  { path: 'lockers', component: LockersComponent, data: { title: 'Bauleras' } },
-  { path: 'finances', component: FinancesComponent, data: { title: 'Finanzas' } },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], data: { title: 'Dashboard' } },
+  { path: 'clients', component: ClientsComponent, canActivate: [AuthGuard], data: { title: 'Clientes' } },
+  { path: 'lockers', component: LockersComponent, canActivate: [AuthGuard], data: { title: 'Bauleras' } },
+  { path: 'finances', component: FinancesComponent, canActivate: [AuthGuard], data: { title: 'Finanzas' } },
   { path: 'communications', component: CommunicationsComponent, data: { title: 'Comunicaciones' } },
-  { path: 'reports', component: ReportsComponent, data: { title: 'Reportes' } },
-  { path: 'statistics', component: StatisticsComponent, data: { title: 'Estadísitcas' } },
-  { path: 'settings', component: SettingsComponent, data: { title: 'Configuración' } },
+  { path: 'reports', component: ReportsComponent, canActivate: [AuthGuard], data: { title: 'Reportes' } },
+  { path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard], data: { title: 'Estadísitcas' } },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard], data: { title: 'Configuración' } },
   
   // Wildcard route to handle not-found URLs
-  { path: '**', redirectTo: '/clients' } 
+  { path: '**', redirectTo: '/login' }
 ];
+
+
